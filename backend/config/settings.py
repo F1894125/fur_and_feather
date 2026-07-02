5572640bc41aee6fe53da3346976e6dfb285f306
spideykook <bedaanntica@gmail.com>
2026-07-02 11:51:33 +0530

    Initialize pets app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pets.apps.PetsConfig',
 ]
 
 MIDDLEWARE = [
