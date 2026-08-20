1eabe20a46a56baf78eafa2991b6e4bf26b723c8
SumanKamila-AI <sumonkamila80@gmail.com>
2026-08-20 23:24:15 +0530

    Update Pet and testimonial html

--- frontend/script.js
+++ frontend/script.js
@@ -208,7 +208,7 @@ $(document).ready(function () {
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
